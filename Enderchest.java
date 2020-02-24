40196bdacd8699812e148caffd0c97f83198718e
Ethan <60994567+Tank-203@users.noreply.github.com>
2020-02-23 19:10:56 -0600

    Update Enderchest.java

--- Enderchest.java
+++ Enderchest.java
@@ -1,14 +1,29 @@
 ****** NOTE: Not Done! ******
 package me.Tank203.Stucker;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.Inventory;
 
 public class Enderchest implements Listener{
+	
+	public static Map<String, List<Inventory>> enderchest = new HashMap<String, List<Inventory>>();
+	
+	//Inventory inv2 = Bukkit.createInventory(null, 36, ChatColor.GREEN + "EnderChest! " + ChatColor.DARK_GRAY + "(Lvl: 2)");
+	//Inventory inv3 = Bukkit.createInventory(null, 54, ChatColor.GREEN + "EnderChest! " + ChatColor.DARK_GRAY + "(MAX)");
+	CommandSender sender;
 	
 	@EventHandler
 	public void onClick(PlayerInteractEvent event) {
@@ -17,7 +32,23 @@ public class Enderchest implements Listener{
 			if (player.getInventory().getItemInMainHand().getItemMeta().getDisplayName().contains("EnderChest"))
 				if (player.getInventory().getItemInMainHand().getItemMeta().hasLore()) 
 					if (event.getAction() == Action.RIGHT_CLICK_AIR) {
-						
+						player.openInventory(Enderchest.einv());
+						sender.sendMessage("playsound minecraft:block.ender_chest.open block " + player);
 					}
+	}
+	
+	@EventHandler
+	public void onPlace(BlockPlaceEvent event) {
+		Player player = (Player) event.getPlayer();
+		if (player.getInventory().getItemInMainHand().getItemMeta().getDisplayName().contains("EnderChest"))
+			if (player.getInventory().getItemInMainHand().getItemMeta().hasLore()) {
+				event.setCancelled(true);
+				player.openInventory(Enderchest.einv());
+			}
+	}
+
+	public static Inventory einv() {
+		Inventory einv = Bukkit.createInventory(null, 27, ChatColor.GREEN + "EnderChest! " + ChatColor.DARK_GRAY + "(Lvl: 1)");
+		return einv;
 	}
 }
