57a406738132c83a34cc364971eb04b2929faff9
Ethan <60994567+Tank-203@users.noreply.github.com>
2020-02-23 21:39:41 -0600

    Update Enderchest.java

--- Enderchest.java
+++ Enderchest.java
@@ -1,13 +1,12 @@
-****** NOTE: Not Done! ******
 package me.Tank203.Stucker;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.Sound;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -17,13 +16,12 @@ import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
 
 public class Enderchest implements Listener{
 	
-	public static Map<String, List<Inventory>> enderchest = new HashMap<String, List<Inventory>>();
-	
-	//Inventory inv2 = Bukkit.createInventory(null, 36, ChatColor.GREEN + "EnderChest! " + ChatColor.DARK_GRAY + "(Lvl: 2)");
-	//Inventory inv3 = Bukkit.createInventory(null, 54, ChatColor.GREEN + "EnderChest! " + ChatColor.DARK_GRAY + "(MAX)");
+	public static Map<String, ItemStack[]> enderchest = new HashMap<String, ItemStack[]>();
+	Inventory einv = Bukkit.createInventory(null, 27, ChatColor.GREEN + "EnderChest!");
 	CommandSender sender;
 	
 	@EventHandler
@@ -33,20 +31,20 @@ public class Enderchest implements Listener{
 			if (player.getInventory().getItemInMainHand().getItemMeta().getDisplayName().contains("EnderChest"))
 				if (player.getInventory().getItemInMainHand().getItemMeta().hasLore()) 
 					if (event.getAction() == Action.RIGHT_CLICK_AIR) {
-						player.openInventory(Enderchest.einv());
-						Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "playsound minecraft:block.ender_chest.open block " + player);
+						if(player.getName().contains(player.getName())) {
+							@SuppressWarnings({ "unlikely-arg-type", "unused" })
+							ItemStack[] inv = enderchest.get(einv);
+							player.openInventory(einv);
+							player.playSound(player.getLocation(), Sound.BLOCK_ENDER_CHEST_OPEN, 3.0F, 0.5F);
+						}
 					}
 	}
 	
 	@EventHandler
 	public void onInvClose(InventoryCloseEvent event) {
-//		Player player = (Player) event.getPlayer();
-		Inventory inv = event.getInventory();
-		
-		if (inv == Enderchest.einv()) {
-			Enderchest.einv().getContents();
-			
-		}
+		Player player = (Player) event.getPlayer();
+			enderchest.put(player.getName(), einv.getContents());
+			player.playSound(player.getLocation(), Sound.BLOCK_ENDER_CHEST_CLOSE, 3.0F, 0.5F);
 	}
 	
 	@EventHandler
@@ -55,13 +53,8 @@ public class Enderchest implements Listener{
 		if (player.getInventory().getItemInMainHand().getItemMeta().getDisplayName().contains("EnderChest"))
 			if (player.getInventory().getItemInMainHand().getItemMeta().hasLore()) {
 				event.setCancelled(true);
-				player.openInventory(Enderchest.einv());
+				player.openInventory(einv);
 				Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "playsound minecraft:block.ender_chest.open block " + player);
 			}
-	}
-
-	public static Inventory einv() {
-		Inventory einv = Bukkit.createInventory(null, 27, ChatColor.GREEN + "EnderChest! " + ChatColor.DARK_GRAY + "(Lvl: 1)");
-		return einv;
 	}
 }
